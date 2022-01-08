7ae10c49ff624a3c86615aa64b0ddc5be8815d46
Muath_Alhajjam <m.alhajjam@nearpay.io>
2022-01-08 12:07:06 +0300

    first commit

--- app/src/main/java/com/example/copypasteapplication/MainActivity.kt
+++ app/src/main/java/com/example/copypasteapplication/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -15,6 +12,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,14 +35,17 @@ data class Message(val author : String, val body: String)
 fun messageCard(msg: Message){
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
+            painter = painterResource(id = R.drawable.my_image),
             contentDescription = "icon",
-            modifier = Modifier.size(60.dp).clip(CircleShape)
+            modifier = Modifier
+                .size(60.dp)
+                .clip(RectangleShape)
         )
 
         Column(modifier = Modifier.padding(all = 8.dp)) {
-            Text(text = msg.author)
-            Text(text = msg.body)
+            Text(text = msg.author,color = Color.White)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = msg.body, color = Color.White)
         }
     }
 }
